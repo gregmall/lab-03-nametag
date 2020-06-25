0e508777b8f44981b95c13d1158a4c6cbcabcad2
Greg Mall <gregmall157@gmail.com>
2020-06-24 20:54:04 -0700

    saving progress

---
+++ app.js
@@ -0,0 +1,21 @@
+const buttonpink= document.getElementById('pink');
+
+buttonpink.addEventListener('click' () => {
+    main.style.background = 'pink';  
+
+    //get the input
+    //get the input value
+    //get the div
+    //set text content of div to input value
+    
+})
+
+const buttongreen= document.getElementById('green');
+buttongreen.addEventListener('click' () => {
+    main.style.background = 'green';
+})
+
+const buttonblue= document.getElementById('blue');
+buttonblue.addEventListener('click' () => {
+    main.style.background = 'blue';
+})
